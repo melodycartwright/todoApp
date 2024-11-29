c94130baaba37716cf57011c302f890a4e4f57db
Melody Cartwright <melody94c@gmail.com>
2024-11-30 00:19:27 +0100

    created config-db.js, fixed .env file and installed mongodb mongoose

--- server-app-exam/src/config/config-db.js
+++ server-app-exam/src/config/config-db.js
@@ -1,2 +1,17 @@
 const mongoose= require ('mongoose');
-const connectDB= async() => {}
+require('dotenv').config();
+const connectDB= async () => {
+    try { await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+        useNewUrlParser:true,
+        useUnifiedTopology:true, 
+    });
+    console.log ('MongoDB connected');
+        
+    } catch (error) {console.error('Error connecting to MongoDB', error.message);
+        throw new Error('failed to connect to MongoDB');
+        
+    }
+
+
+};
+module.exports = connectDB;
